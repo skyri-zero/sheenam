a944d80f26422cb570e03bf5075d3ad40e099416
Olyvar Camomile <pulat5212@gmail.com>
2025-03-12 11:11:59 +0500

    INFRA: Initialize Build Project

---
+++ Sheenam.Api.Infrastructure.Build/Program.cs
@@ -0,0 +1,71 @@
+﻿using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+var githubPipeline = new GithubPipeline
+{
+    Name = "Sheenam Build Pipeline",
+
+    OnEvents = new Events
+    {
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new string[] { "main" }
+        },
+
+        Push = new PushEvent
+        {
+            Branches = new string[] { "main" }
+        }
+    },
+
+    Jobs = new Dictionary<string, Job>
+    {
+        {
+            "build",
+            new Job
+            {
+                RunsOn = BuildMachines.Windows2022,
+
+                Steps = new List<GithubTask>
+                {
+                    new CheckoutTaskV2
+                    {
+                        Name = "Checking Out Code"
+                    },
+
+                    new SetupDotNetTaskV1
+                    {
+                        Name = "Setting Up .Net",
+                        TargetDotNetVersion = new TargetDotNetVersion
+                        {
+                            DotNetVersion = "8.0.406"
+                        }
+                    },
+
+                    new RestoreTask
+                    {
+                        Name = "Restoring NuGet Packages"
+                    },
+
+                    new DotNetBuildTask
+                    {
+                        Name = "Building Project"
+                    },
+
+                    new TestTask
+                    {
+                        Name = "Running Tests"
+                    }
+                }
+            }
+        }
+    }
+};
+
+var client = new ADotNetClient();
+
+client.SerializeAndWriteToFile(
+    adoPipeline: githubPipeline,
+    path: "../../../../.github/workflows/dotnet.yml");
