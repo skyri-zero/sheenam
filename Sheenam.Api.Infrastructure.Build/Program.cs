7b737ebc2451d3744ea42d1705c25067dca4078d
Sherzod Pulatov <71495327+skyri-zero@users.noreply.github.com>
2025-03-12 11:23:08 +0500

    Update Program.cs
    Added header

--- Sheenam.Api.Infrastructure.Build/Program.cs
+++ Sheenam.Api.Infrastructure.Build/Program.cs
@@ -1,4 +1,9 @@
-﻿using ADotNet.Clients;
+//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+using ADotNet.Clients;
 using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
 using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
 using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
